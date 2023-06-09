93909d71d3b60cbc6f4e759b6153990396f156ad
Kevin Musembi <kevinmusembi.m@gmail.com>
2023-06-09 11:45:50 -0700

    Static Library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _atoi - converts string to integer
+* @s: input
+*
+* Return: 0
+*/
+int _atoi(char *s)
+{
+	;
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strspn - string function
+* @s: first input
+* @accept: second input
+*
+* Return: 0
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	;
+}
